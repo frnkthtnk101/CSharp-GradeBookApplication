b73d176fdea122d38a1459a6e45922c045ef4da9
Franco Pettigrosso <fpettigrosso@holyfamily.edu>
2019-01-18 14:32:23 -0500

    the base is now abstract

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -8,13 +8,13 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class BaseGradeBook
+    public abstract class BaseGradeBook
     {
         public GradeBookType Type { get; set; }   
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
-        public BaseGradeBook(string name)
+        protected public BaseGradeBook(string name)
         {
             Name = name;
             Students = new List<Student>();
